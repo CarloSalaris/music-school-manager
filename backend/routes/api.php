5202a6c590063f8f8ae1907af9e2e2d3a6cf3b7f
CarloSalaris <carloa.salaris@gmail.com>
2026-05-11 15:11:10 +0200

    feat(backend): configure API routes

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/students/index', [App\Http\Controllers\Api\StudentsController::class, 'index']);
